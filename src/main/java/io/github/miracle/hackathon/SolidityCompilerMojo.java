58083e4866e06b49e06e58e1ea6b41b0d448882b
vanduc2514 <vanduc2514@gmail.com>
2023-08-12 21:45:28 +0700

    refactor get Solc Arguments

--- src/main/java/io/github/miracle/hackathon/SolidityCompilerMojo.java
+++ src/main/java/io/github/miracle/hackathon/SolidityCompilerMojo.java
@@ -6,6 +6,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.jetbrains.annotations.NotNull;
 import org.web3j.sokt.*;
 
 import java.io.IOException;
@@ -32,8 +33,8 @@ public class SolidityCompilerMojo extends AbstractMojo {
     @Parameter(name = "generateAbi", defaultValue = "true")
     protected boolean generateAbi;
 
-    @Parameter(name = "solcArguments")
-    protected Map<String, String> solcArguments = new HashMap<>();
+    @Parameter(name = "overrideSolcArgs")
+    protected Map<String, String> overrideSolcArgs;
 
     @Parameter(name = "web3jLocation", defaultValue = ".web3j")
     protected String web3jLocation;
@@ -44,14 +45,19 @@ public class SolidityCompilerMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (!generateAbi && !generateBin) return;
-        List<SolidityFile> solidityFiles = new ArrayList<>();
+        getLog().info("Compile Contract(s)  " + inputContracts);
+        compileContractsToOutput();
+    }
+
+    protected void compileContractsToOutput() throws MojoExecutionException {
+        List<SolidityFile> inputSolidityContracts = new ArrayList<>();
         for (String path : inputContracts) {
-            solidityFiles.add(new SolidityFile(path));
+            inputSolidityContracts.add(new SolidityFile(path));
         }
         if (solcVersion == null) {
-            solcVersion = solidityFiles.get(0).getVersionPragma();
+            solcVersion = inputSolidityContracts.get(0).getVersionPragma();
         }
-        compileSolidityContract(solcVersion, solidityFiles, tempLocation);
+        compileSolidityContract(solcVersion, overrideSolcArgs, inputSolidityContracts, tempLocation);
         moveToOutputFolder();
     }
 
@@ -70,23 +76,24 @@ public class SolidityCompilerMojo extends AbstractMojo {
 
     protected void compileSolidityContract(
             String solcVersion,
+            Map<String, String> overrideArgs,
             List<SolidityFile> inputContracts,
             String outputPath) throws MojoExecutionException {
-        getLog().info("Compile Contract(s)  " + inputContracts);
         SolcInstance contractCompiler = new SolcInstance(
                 getSolcRelease(solcVersion),
                 web3jLocation,
                 false,
                 inputContracts.toArray(new SolidityFile[0]));
         SolcRelease releaseMetadata = contractCompiler.getSolcRelease();
+        String solcRootPath = getSolcRootPath();
         if (contractCompiler.installed()) {
             getLog().info("Found a compatible Solidity Compiler (" +
-                    releaseMetadata.getVersion() + ")");
+                    releaseMetadata.getVersion() + ")" + " in " + solcRootPath);
         } else {
             getLog().info("This process will install a compatible Solidity Compiler (" +
-                    releaseMetadata.getVersion() + ")" + " in " + getSolcRootPath());
+                    releaseMetadata.getVersion() + ")" + " in " + solcRootPath);
         }
-        Set<SolcArguments> compilerArguments = getCompilerArguments(outputPath);
+        Set<SolcArguments> compilerArguments = getCompilerArguments(overrideArgs, outputPath);
         SolcOutput compilationOutput = contractCompiler.execute(compilerArguments.toArray(new SolcArguments[0]));
         if (compilationOutput.getExitCode() == 0) {
             getLog().info(compilationOutput.getStdOut());
@@ -103,21 +110,6 @@ public class SolidityCompilerMojo extends AbstractMojo {
         return solcRelease;
     }
 
-    private Set<SolcArguments> getCompilerArguments(String outputPath) {
-        Set<SolcArguments> compilerArguments = getCompilerArguments(solcArguments);
-        List<SolcArguments> requiredArguments = List.of(
-                SolcArguments.BIN,
-                SolcArguments.ABI,
-                SolcArguments.OVERWRITE,
-                SolcArguments.OUTPUT_DIR.param(() -> outputPath));
-        for (SolcArguments argument : requiredArguments) {
-            if (!compilerArguments.add(argument)) {
-                getLog().debug(argument.getBaseArg() + "is override by compiler argument");
-            }
-        }
-        return compilerArguments;
-    }
-
     private String getSolcRootPath() {
         return Paths.get(
                 System.getProperty("user.home"),
@@ -125,7 +117,21 @@ public class SolidityCompilerMojo extends AbstractMojo {
                 "solc").toString();
     }
 
-    private static Set<SolcArguments> getCompilerArguments(Map<String, String> solcArguments) {
+    private static Set<SolcArguments> getCompilerArguments(Map<String, String> overrideArgs, String outputPath) {
+        if (overrideArgs != null) return getOverrideArguments(overrideArgs);
+        return getDefaultArguments(outputPath);
+    }
+
+    @NotNull
+    private static Set<SolcArguments> getDefaultArguments(String outputPath) {
+        return Set.of(
+                SolcArguments.BIN,
+                SolcArguments.ABI,
+                SolcArguments.OVERWRITE,
+                SolcArguments.OUTPUT_DIR.param(() -> outputPath));
+    }
+
+    private static Set<SolcArguments> getOverrideArguments(Map<String, String> solcArguments) {
         Set<SolcArguments> compilerArguments = new HashSet<>();
         Map<String, SolcArguments> simplifiedBaseArgs = getSimplifiedBaseArgs();
         solcArguments.forEach((argName, argValue) -> {
